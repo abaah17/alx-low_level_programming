4b4eac674f89c3f6f44a74615c8a56f456b14f47
Anthony Baah <abaah17@alustudent.com>
2020-09-17 20:16:26 +0200

    Conditional statements

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,21 +1,31 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - to tell if a random number is either - or +
+ *
+ * n is the random integer
+ *
+ * Return: the integer
+ */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if(n > 0){
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    /* your code goes there */
+    if(n > 0)
+    {
         printf("%d is positive\n", n);
-	}else if(n = 0 ){
-	    printf("n is zero\n", n);
-	}else{
+    }
+    else if(n ==0 ){
+        printf("%d is zero\n", n);
+    }else{
         printf("%d is negative\n", n);
-	}
-	return (0);
+    }
+    return (0);
 }
