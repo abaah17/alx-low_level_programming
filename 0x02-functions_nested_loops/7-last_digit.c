2815b4b6c7d213915e3c3fb961d3bc5973284667
Anthony Baah <abaah17@alustudent.com>
2020-09-22 19:39:14 +0200

    Last digit

--- 0x02-functions_nested_loops/7-last_digit.c
+++ 0x02-functions_nested_loops/7-last_digit.c
@@ -1,22 +1,20 @@
 #include "holberton.h"
 /**
- *print_last_digit - prints the last digit of the parameter
- *@n: parameter passed in the function
- *Description: the function returns the last digit
- *Return: Always(0) Success
+ * print_last_digit - prints the lest digit
+ * @n: parameter
+ *
+ * Description: prints the last digit
+ * Return: Always(0) Success
  */
-
 int print_last_digit(int n)
 {
 	int lst_digit;
 
-	if (n >= 0)
+	lst_digit = n % 10;
+
+	if (lst_digit < 0)
 	{
-		lst_digit = n % 10;
-	}
-	else
-	{
-		lst_digit = (n % 10) * -1;
+		lst_digit *= -1;
 	}
 	_putchar(lst_digit + '0');
 	return (lst_digit);
