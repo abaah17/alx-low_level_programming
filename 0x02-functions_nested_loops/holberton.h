8251589197f1ffe95bc97558c36516a2f12c64c0
Anthony Baah <abaah17@alustudent.com>
2020-09-22 12:29:59 +0200

    alphabets

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,7 +1,9 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
 int _putchar(char c);
 void print_alphabet(void);
-
-#endif
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
