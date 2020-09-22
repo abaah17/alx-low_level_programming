112dbed90477bbb094df0fba3c83d3c7eabcf393
Anthony Baah <abaah17@alustudent.com>
2020-09-22 13:23:53 +0200

    Alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_alphabet - prints all aphabets in lowercase
+ *                  10X
+ *
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char start_alphabet = 'a', end_alphabet;
+	while (start_alphabet < 'k')
+	{
+		end_alphabet = 'a';
+		while (end_alphabet <= 'z')
+		{
+			putchar(end_alphabet);
+			end_alphabet++;
+		}
+		putchar('\n');
+		start_alphabet++ ;
+	}
+}
