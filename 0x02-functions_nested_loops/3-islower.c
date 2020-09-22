cb1e46aa5c58dbc085047f0eadf47b93cbd68601
Anthony Baah <abaah17@alustudent.com>
2020-09-22 15:05:36 +0200

    Islower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks if the parameter is lowercase
+ *@c: character to be checked by function
+ * Description:
+ * Return: Always(0) Success
+ */
+
+int _islower(int c)
+{
+	if (c >= 97)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
