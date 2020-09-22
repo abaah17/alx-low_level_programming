3859a9788bb86f45433e16d8a8b0f4ce086dff35
Anthony Baah <abaah17@alustudent.com>
2020-09-22 18:16:55 +0200

    Absolute function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *_abs - prints the absolute value of the parameter
+ *@n: parameter passed in the function
+ *Description: the function checks if the parameter is absolute
+ *Return: Always(0) Success
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= 1;
+		return (n);
+	}
+	else
+	{
+		n *= 1;
+		return (n);
+	}
+}
