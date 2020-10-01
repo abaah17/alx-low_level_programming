2c94fb1d43644b068941554f80b1c460d6b4ed55
Anthony Baah <abaah17@alustudent.com>
2020-10-01 12:34:35 +0200

    Reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - it takes a pointer int and update the
+ *               value.
+ * @n : The pointer parameter
+ * Return: Void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
