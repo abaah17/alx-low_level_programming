59e9c7ac958f812dc18d2ea217a7f2f0c89e5e30
Anthony Baah <abaah17@alustudent.com>
2020-10-02 22:59:02 +0200

    Pointres

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *_strncpy - copies a string.
+ *@dest:destination string.
+ *@src:source string.
+ *@n:size to copy from src.
+ *Return: pointer to dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
