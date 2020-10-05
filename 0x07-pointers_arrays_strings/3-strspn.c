49445d465daf0de0018146cf84f0069edcdf4bac
Anthony Baah <abaah17@alustudent.com>
2020-10-05 22:26:50 +0200

    Pointer

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * _strspn - gets len of prefix substring
+ * @s: first pointer parameter
+ * @accept: second pointer parameter
+ *
+ * Return: Always(0) Success
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (accept[j] == s[i])
+			{
+				break;
+				j++;
+			}
+
+		}
+		if (!accept[j])
+		{
+			break;
+		}
+		i++;
+	}
+	return (i);
+}
