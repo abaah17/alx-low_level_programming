bd6e59d875e7a5b24bf83c5528a19965c246fd8c
Anthony Baah <abaah17@alustudent.com>
2020-10-05 22:42:31 +0200

    Pointer

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,10 +14,10 @@ char *_strpbrk(char *s, char *accept)
 	int i, j;
 
 	i = 0;
-	while (s[i] != NULL)
+	while (s[i] != '\0')
 	{
 		j = 0;
-		while (accept[j] != NULL)
+		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
 			{
