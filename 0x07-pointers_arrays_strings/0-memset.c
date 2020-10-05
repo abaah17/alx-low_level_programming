7bbf5e7f9013a8f62b2c8bf4af59f15180d29741
Anthony Baah <abaah17@alustudent.com>
2020-10-05 21:28:02 +0200

    Pointer

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _memset - replace n characters with b
+ * @s: memory address for the string value
+ * @b: the value to replace the n characters
+ * @n: the number of characters to replace
+ *
+ * Description:n chars are replaced
+ * Return: Always(0) Success
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	char *index = (char *)s;
+
+	for (i = 0; i < n; i++)
+		index[i] = b;
+
+	return (index);
+}
