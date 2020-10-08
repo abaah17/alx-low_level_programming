1579832742d9235104f742f6ff8a92db348eb63d
Anthony Baah <abaah17@alustudent.com>
2020-10-08 14:34:54 +0200

    Puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,10 +6,8 @@ include "holberton.h"
  */
 void _puts(char *str)
 {
-	while (*str)
+	for (int i = 0; str[i] != '\0'; ++i)
 	{
-		_putchar(*str);
-		str++;
+		_putchar(str[i]);
 	}
-	_putchar('\n');
 }
