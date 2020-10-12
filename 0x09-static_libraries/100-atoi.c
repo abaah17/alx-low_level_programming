2e6ad3dbbe7f038ffbfeaa2eda9067740b1f2e0c
Anthony Baah <abaah17@alustudent.com>
2020-10-12 22:26:56 +0200

    Static Lib

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _atoi - convert a string to an integer.
+ * @s: the string to be comverted.
+ *
+ *Return: The integer value of the string converted
+ */
+
+int _atoi(char *s)
+{
+
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ * @dest: character destination pointer
+ * @src: character source pointer
+ *Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+}
