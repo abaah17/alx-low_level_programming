003dda72c05c56b61ca15e363ed23e086dea9559
Anthony Baah <abaah17@alustudent.com>
2020-11-12 21:41:17 +0200

    File I/0

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+include "holberton.h"
 
 /**
  * main - copies the content of a file to another file
@@ -9,21 +9,21 @@
 
 int main(int argc, char *argv[])
 {
-	int f1, f2, _read, ch_in, ch_out;
+	int _file1, _file2, _read, c1, c2;
 	char buffer[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
-	f1 = open(argv[1], O_RDONLY);
-	if (f1 < 0)
+	_file1 = open(argv[1], O_RDONLY);
+	if (_file1 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	_file2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
-	while ((_read = read(f1, buffer, 1024)) > 0)
+	while ((_read = read(_file1, buffer, 1024)) > 0)
 	{
 		if (_file2 < 0 || (write(_file2, buffer, _read) != _read))
 		{
@@ -35,13 +35,13 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	ch_in = close(f1);
-	if (ch_in < 0)
+	c1 = close(_file1);
+	if (c1 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", f1), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", _file1), exit(100);
 	}
-	ch_out = close(_file2);
-	if (ch_out < 0)
+	c2 = close(_file2);
+	if (c2 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", _file2), exit(100);
 	}
